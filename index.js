ca27765548e0546cc1850c02df8720499b3a3773
Madeleine Francis <madeleinefrncs@gmail.com>
2019-01-30 18:11:04 -0800

    fig view now has add person modal that needs styling

--- index.js
+++ index.js
@@ -11,8 +11,18 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 app.use(require('./stores/clicks'))
+app.use(require('./stores/modals'))
+app.use(function (state) {
+  // initialize state
+  state.people = {}
+  state.meals = {}
+  state.personModal = 'invisible'
+  state.mealModal = 'hidden'
+})
+
 
 app.route('/', require('./views/main'))
-app.route('/*', require('./views/404'))
+app.route('/fig', require('./views/fig'))
+// app.route('/*', require('./views/404'))
 
 module.exports = app.mount('body')
