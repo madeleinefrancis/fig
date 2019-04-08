46fac00dcaa1cc3898198104148aced4381ab965
Madeleine Francis <madeleinefrncs@gmail.com>
2019-04-07 17:05:33 -0700

    tabs now working on meals view - need to fill with correct content

--- components/mealsview.js
+++ components/mealsview.js
@@ -6,25 +6,40 @@ module.exports = function (state, emit) {
   // create html template
   	return html`
   		<div class="modal-wrapper ${state.mealsView}">
-  			<div class="modal-content-wrapper">
-  				MEALS
-  			</div>
-  			<div id="meal-columns-wrapper">
-	  			<div id="member-columns">
-		    		<div class="column">
-		  				<div id="dated-meals" class="meal-column">
-		  					${state.mealsWDatesIDs.map(datedMealRow)}
-		  				</div>
-		    		</div>
-		    		<div class="column">
-		  				<div id="non-dated-meals" class="meal-column">
-		  					${state.alphaMealIDs.map(datedMealRow)}
-		  				</div>
-		    		</div>
-		    	</div>
-  			</div>
+  			<ul>
+          <li onclick=${switchTab} id="tb_1" index="1" class="tabmenu active">Tab 1</li>
+          <li onclick=${switchTab} id="tb_2" index="2" class="tabmenu">Tab 2</li>
+        </ul>
+         
+        <div id="content_1" index="1" class="tabcontent"> 
+          Content of the first tab.
+        </div> 
+        <div id="content_2" index="2" class="tabcontent" style="display:none;">
+          Content of the second tab.
+        </div>
   		</div>
   		`
+
+      function switchTab(event) {
+        // first of all we get all tab content blocks (I think the best way to get them by class names)
+        var tabID = event.target.getAttribute("id")
+        var tabIndex = event.target.getAttribute("index")
+        var x = document.getElementsByClassName("tabcontent");
+        var i;
+        for (i = 0; i < x.length; i++) {
+          x[i].style.display = 'none'; // hide all tab content
+        }
+        var contentID = "content_" + tabIndex
+        document.getElementById(contentID).style.display = 'block'; // display the content of the tab we need
+       
+        // now we get all tab menu items by class names (use the next code only if you need to highlight current tab)
+        var x = document.getElementsByClassName("tabmenu");
+        var i;
+        for (i = 0; i < x.length; i++) {
+          x[i].className = 'tabmenu'; 
+        }
+        document.getElementById(tabID).className = 'tabmenu active';
+      }
 
   		function datedMealRow (id) {
   			var formatedDate = formatDate(id)
@@ -77,4 +92,22 @@ module.exports = function (state, emit) {
   			emit('display meal', mealID)
   		}
 }
+
+// <div class="modal-content-wrapper">
+//           MEALS
+//         </div>
+//         <div id="meal-columns-wrapper">
+//           <div id="member-columns">
+//             <div class="column">
+//               <div id="dated-meals" class="meal-column">
+//                 ${state.mealsWDatesIDs.map(datedMealRow)}
+//               </div>
+//             </div>
+//             <div class="column">
+//               <div id="non-dated-meals" class="meal-column">
+//                 ${state.alphaMealIDs.map(datedMealRow)}
+//               </div>
+//             </div>
+//           </div>
+//         </div>
 
